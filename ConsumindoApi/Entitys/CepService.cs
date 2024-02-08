34e66a7797b8f94111c52ae2967828c4eb8dc3f9
Noonato <101616181+Noonato@users.noreply.github.com>
2024-02-08 10:41:11 -0300

    Add files via upload

---
+++ ConsumindoApi/Entitys/CepService.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsumindoApi.Entitys
+{
+    public class CepService
+    {
+        public async Task<CepModel> Integracao(int cep)
+        {
+            HttpClient httpClient = new HttpClient();
+            var response = await httpClient.GetAsync($"https://viacep.com.br/ws/{cep}/json");
+            var jsonString = await response.Content.ReadAsStringAsync();
+
+            var jsonObject = JsonConvert.DeserializeObject<CepModel>(jsonString);
+
+            if (jsonObject != null)
+            {
+                return jsonObject;
+            }
+            else
+            {
+               return new CepModel
+                {
+                    Verificacao = true
+                };
+            }
+
+
+
+        }
+    }
+    }
+
---
+++ ConsumindoApi/Program.cs
@@ -0,0 +1,46 @@
+﻿using ConsumindoApi.Entitys;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsumindoApi
+{
+     class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            Console.WriteLine("Informe o CEP:");
+            int cep = int.Parse(Console.ReadLine());
+
+            CepService cepServices = new CepService();
+
+            CepModel cepEncontrado = await cepServices.Integracao(cep);
+
+            if (!cepEncontrado.Verificacao)
+            {
+                Console.WriteLine("o Cep foi encontrado");
+
+                Console.WriteLine($"Cep:{cepEncontrado.Cep}");
+                Console.WriteLine($"Logradouro:{cepEncontrado.Logradouro}");
+                Console.WriteLine($"Complemento:{cepEncontrado.Complemento}");
+                Console.WriteLine($"Bairro:{cepEncontrado.Bairro}");
+                Console.WriteLine($"Localidade:{cepEncontrado.Localidade}");
+                Console.WriteLine($"UF:{cepEncontrado.UF}");
+                Console.WriteLine($"IBGE:{cepEncontrado.IBGE}");
+                Console.WriteLine($"GIA:{cepEncontrado.GIA}");
+                Console.WriteLine($"DDD:{cepEncontrado.DDD}");
+                Console.WriteLine($"Siafi:{cepEncontrado.Siafi}");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine($"O cep {cep} não foi encontrado");
+            }
+
+
+        }
+    }
+}
